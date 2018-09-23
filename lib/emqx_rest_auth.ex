a02793fbd169651a9d802f42cab92526d566d7e4
Sakib Sami <s4kibs4mi@gmail.com>
2018-09-23 16:46:13 +0600

    - Added : Auth/Acl

--- lib/emqx_rest_auth.ex
+++ lib/emqx_rest_auth.ex
@@ -3,10 +3,12 @@ defmodule EmqxRestAuth do
 
   def start(_type, _args) do
     EmqxRestAuth.Cli.load([])
+    EmqxRestAuth.Acl.load([])
     EmqxRestAuth.Supervisor.start_link()
   end
 
   def stop(_app) do
     EmqxRestAuth.Cli.unload()
+    EmqxRestAuth.Acl.unload()
   end
 end
---
+++ lib/emqx_rest_auth/emqx_rest_auth_acl.ex
@@ -0,0 +1,36 @@
+defmodule EmqxRestAuth.Acl do
+
+  def load(env) do
+    :emqttd_access_control.register_mod(:auth, &EmqxRestAuth.Acl, [])
+    :emqttd_access_control.register_mod(:acl, &EmqxRestAuth.Acl, [])
+  end
+
+  def unload do
+    :emqttd_access_control.unregister_mod(:auth, &EmqxRestAuth.Acl)
+    :emqttd_access_control.unregister_mod(:acl, &EmqxRestAuth.Acl)
+  end
+
+  def init(_opts) do
+    IO.puts("On Auth/Acl init")
+    {:ok, _opts}
+  end
+
+  def check(client, password, _opts) do
+    IO.puts("OnAuth check")
+    {:ok}
+  end
+
+  def check_acl(client, _opts) do
+    IO.puts("OnAcl check_acl")
+    {:ok, :allow}
+  end
+
+  def reload_acl(_opts) do
+    IO.puts("OnReloadAcl reload_acl")
+    {:ok}
+  end
+
+  def description() do
+    "Emqx Res Auth/Acl Description"
+  end
+end
